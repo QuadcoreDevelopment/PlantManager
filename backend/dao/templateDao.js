97fffe4f8b7c9b2899871886619de8f14e77c911
TOBIS-PC-RYZEN\tobia <tobiasgraf03@gmail.com>
2025-04-18 22:09:08 +0200

    #15 template Dao

---
+++ backend/dao/templateDao.js
@@ -0,0 +1,59 @@
+// load helper
+const helper = require('../helper.js');
+// load other DAOs if needed
+//example: const LandDao = require('./landDao.js');
+
+class templateDao {
+
+    constructor(dbConnection) {
+        this._conn = dbConnection;
+    }
+
+    getConnection() {
+        return this._conn;
+    }
+
+    loadById(id) {
+
+        // Code goes here
+
+        return result;
+    }
+
+    loadAll() {
+        // Code goes here
+
+        return result;
+    }
+
+    exists(id) {
+
+        // Code goes here
+
+        //return true/false;
+    }
+
+    create(strasse = '', hausnummer = '', adresszusatz = '', plz = '', ort = '', landId = 1) {
+        
+        // Code goes here
+
+        return this.loadById(result.lastInsertRowid);
+    }
+
+    update(id, strasse = '', hausnummer = '', adresszusatz = '', plz = '', ort = '', landId = 1) {
+
+        // Code goes here
+
+        return this.loadById(id);
+    }
+
+    delete(id) {
+        // Code goes here
+    }
+
+    toString() {
+        console.log('templateDao [_conn=' + this._conn + ']');
+    }
+}
+
+module.exports = templateDao;
