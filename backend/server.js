919e0c76c1871b32422fab34d2ecf4f7a4a48ddf
CoderTobi <77673526+CoderTobi@users.noreply.github.com>
2025-04-18 21:20:45 +0200

    DB Name gändert

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ try
 	console.log('Connect database...');
 	const Database = require('better-sqlite3');
 	const dbOptions = { verbose: console.log };
-	const dbFile = './db/webanw2.sqlite';
+	const dbFile = './db/plantmandb.sqlite';
 	const dbConnection = new Database(dbFile, dbOptions);
 
 	// create server
