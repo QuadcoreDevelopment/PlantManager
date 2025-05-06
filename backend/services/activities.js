6e778921f558aba9856ea1e4c9c0d716e2e6a541
ShinyEmeraldGames <spatzenarmy@gmx.de>
2025-05-06 16:47:25 +0200

    #24 Skelett für Activity Service erstellt

--- backend/services/activities.js
+++ backend/services/activities.js
@@ -5,6 +5,40 @@ var serviceRouter = express.Router();
 
 console.log('- Service Activities');
 
-// Hier Inhalt
+// Neue Activity erstellen
+serviceRouter.post('/activities', function(request, response) {
+    try {
+        //
+    } catch (error) {
+        //
+    }
+});
+
+// Activity nach ID holen
+serviceRouter.get('/activities/exists/:id', function(request, response) {
+    try {
+        //
+    } catch (error) {
+        //
+    }
+});
+
+// Alle Activities holen
+serviceRouter.get('/activities/all/', function(request, response) {
+    try {
+        //
+    } catch (error) {
+        //
+    }
+});
+
+// Activity löschen
+serviceRouter.delete('/activities/:id', function(request, response) {
+    try {
+        //
+    } catch (error) {
+        //
+    }
+});
 
 module.exports = serviceRouter;
