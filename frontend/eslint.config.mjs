9a98649a313aae8b31f99f69db2b89fc5e2df9b6
CoderTobi <77673526+CoderTobi@users.noreply.github.com>
2025-07-24 00:59:21 +0200

    #54 EsLint Config for the frontend with JQuerry support

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,33 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+
+export default defineConfig([
+	{ files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+	{ files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+	{ files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+	{
+		"rules": {
+
+			"no-unused-vars": ["warn", {
+				"vars": "all",
+				"args": "after-used",
+				"caughtErrors": "all",
+				"ignoreRestSiblings": false,
+				"reportUsedIgnorePattern": false
+			}],
+
+			"no-use-before-define": ["error", {
+				"functions": true,
+				"classes": true,
+				"variables": true,
+				"allowNamedExports": false
+			}]
+		},
+		languageOptions: { 
+			globals: { ...globals.browser, ...globals.jquery }
+		}
+	}
+
+]);
