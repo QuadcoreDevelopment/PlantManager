19e58ebd2d4ab6c022ace763b7e7b1f8fd1af89a
CoderTobi <77673526+CoderTobi@users.noreply.github.com>
2025-07-25 22:06:37 +0200

    #50 new centralized error handler WIP

---
+++ frontend/public/mjs/error_handler.mjs
@@ -0,0 +1,50 @@
+import * as alerts from "./alerts.mjs";
+
+/**
+ * Takes an error aka. exception, generates a user-friendly message for it and displays it.
+ * @param {Error} error The Error for which a message should be displayed.
+ */
+export function handleError(error) {
+    console.error("Handling error:", error);
+
+    // generate main message
+    let mainMessage = "Ein unbekannter Fehler ist aufgetreten";
+
+    switch (error.name) {
+        case "TypeError":
+            if (error.message.includes("NetworkError")) {
+                mainMessage = "Fehler bei der Kommunikation mit dem Backend";
+            }
+            break;
+    
+        default:
+            if (error.message.includes("Failed to fetch plants")) {
+                mainMessage = "Fehler beim Abrufen der Pflanzen";
+            } else if (error.message.includes("Failed to create plant")) {
+                mainMessage = "Konnte keine neue Pflanze hinzufügen";
+            } else if (error.message.includes("Failed to create activity")) {
+                if(error.message.includes("type 0"))
+                {
+                    mainMessage = "Konnte die Pflanze nicht bewässern";
+                }
+                else if(error.message.includes("type 1"))
+                {
+                    mainMessage = "Konnte die Pflanze nicht umtopfen";
+                }
+                else{
+                    mainMessage = "Konnte keine neue Aktivität anlegen";
+                }
+            }
+            break;
+    }
+
+    // generate secondary message
+    let secondaryMessage = null;
+
+    if (error.message.includes("Error 404")) {
+        secondaryMessage = "Das Backend unterstützt die benötigte API nicht. Möglicherweise wird ein Update benötigt.";
+    }
+
+    // Display the error message
+    alerts.displayError(mainMessage, secondaryMessage);
+}
