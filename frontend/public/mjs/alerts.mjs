ec50b7ee2be5a1a07e13b1d9c37e63426643344f
CoderTobi <77673526+CoderTobi@users.noreply.github.com>
2025-07-25 22:05:24 +0200

    #50 reworked alerts display

--- frontend/public/mjs/alerts.mjs
+++ frontend/public/mjs/alerts.mjs
@@ -14,10 +14,19 @@ export function initializeAlertDisplay()
  * Must be called after initializeAlertDisplay().
  * 
  * @param {string} message a string that will be displayed to the user 
- * @param {string} type a string to change the apperance based on Bootsrap (e.g. danger) 
+ * @param {string} type a string to change the apperance based on Bootsrap (e.g. danger)
+ * @param {string} secondaryMessage (optional) additional information that will be displayed to the user 
  */
-export function displayAlert(message, type) {
+export function displayAlert(mainMessage, type, secondaryMessage) {
 	
+    let message = mainMessage;
+	// Append secondary message
+    if(secondaryMessage != null)
+    {
+        message = "<strong>" + message + "</strong>";
+		message += "<br>" + secondaryMessage;
+    }
+
 	let alert = $('<div class="alert alert-' + type + ' alert-dismissible" role="alert">');
 	alert.append($('<div>'+ message + '</div>'));
 	alert.append($('<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>'));
@@ -29,8 +38,10 @@ export function displayAlert(message, type) {
  * Displays the message as in red to the user.
  * Must be called after initializeAlertDisplay().
  * 
- * @param {string} message a string that will be displayed to the user 
+ * @param {string} mainMessage a string that will be displayed to the user 
+ * @param {string} secondaryMessage (optional) additional information that will be displayed to the user 
  */
-export function displayError(message) {
-	displayAlert(message, "danger");
+export function displayError(mainMessage, secondaryMessage) {
+	
+	displayAlert(mainMessage, "danger", secondaryMessage);
 }
