f516ce40d065cdae50cb0b2a5d557f1d106bd8c3
CoderTobi <77673526+CoderTobi@users.noreply.github.com>
2025-09-05 22:41:00 +0200

    #63 added new column to DB

--- backend/dbMigrationTool.js
+++ backend/dbMigrationTool.js
@@ -4,7 +4,20 @@
  * @returns {boolean} false if the DB is fine and true if it needs to be migrated
  */
 module.exports.dbNeedsMigration = function(dbConnection) {
-    // TODO implement this when necessary
+    // ============== Check requirement for v1.0.1 ==============
+    const columnExists = dbConnection.prepare(`
+        PRAGMA table_info(plants)
+    `).all().some(col => col.name === 'composted');
+
+    if(!columnExists)
+    {
+        return true;
+    }
+
+    // ============== Check requirement for v.. ==============
+    // ...
+
+    // All good
     return false;
 }
 
@@ -13,5 +26,21 @@ module.exports.dbNeedsMigration = function(dbConnection) {
  * @param {*} dbConnection the connection to the DB
  */
 module.exports.migrateDB = function(dbConnection) {
-    // TODO implement this when necessary
+    // ============== Upgrade from v1.0.0 to v1.1.0 ==============
+    // Add 'composted' column to 'plants' table if it does not exist
+    const columnExists = dbConnection.prepare(`
+        PRAGMA table_info(plants)
+    `).all().some(col => col.name === 'composted');
+
+    if (!columnExists) {
+        dbConnection.prepare(`
+            ALTER TABLE plants ADD COLUMN composted DATE DEFAULT NULL
+        `).run();
+        console.log("Column 'composted' added to 'plants' table.");
+    } else {
+        console.log("Column 'composted' already exists in 'plants' table.");
+    }
+
+    // ============== Upgrade from v1.0.0 to v.. ==============
+    // ...
 }
