dc5305a077e6246cec7249ca3742cdc9480cf990
CoderTobi <77673526+CoderTobi@users.noreply.github.com>
2025-10-14 23:32:49 +0200

    #51 updated replaceMiddleware to support multiple markers

--- frontend/middleware/replaceMiddleware.js
+++ frontend/middleware/replaceMiddleware.js
@@ -1,6 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 
+// Mapping: Marker => Datei
+const replacements = {
+    '<!-- nav -->': path.join(__dirname, '../templates/nav.html'),
+    '<!-- commonHead -->': path.join(__dirname, '../templates/commonHead.html'),
+    '<!-- bottomScripts -->': path.join(__dirname, '../templates/bottomScripts.html'),
+    // weitere Marker und Dateien hier ergänzen
+};
+
 function replaceMiddleware(req, res, next) {
     let requestedFile = req.path;
 
@@ -9,27 +17,53 @@ function replaceMiddleware(req, res, next) {
         requestedFile = '/index.html';
     }
 
-    if (requestedFile.endsWith('.html')) {
-        console.log("replaceMiddleware");
-        const filePath = path.join(__dirname, '../public', requestedFile);
-        fs.readFile(filePath, 'utf8', (err, data) => {
-            if (err) {
-                console.log("Error in replaceMiddleware: could not find " + filePath);
-                return next();
+    // Wenn es kein HTML ist skip
+    if (!requestedFile.endsWith('.html')) {
+        return next();
+    }
+
+    console.log(`replaceMiddleware running for ${requestedFile}`);
+
+    const filePath = path.join(__dirname, '../public', requestedFile);
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.log("Error in replaceMiddleware: could not find " + filePath);
+            return next();
+        }
+
+        // Alle Marker ersetzen
+        const markers = Object.keys(replacements);
+        let pending = markers.length;
+        let replacedData = data;
+
+        if (pending === 0) {
+            res.setHeader('Content-Type', 'text/html');
+            return res.send(replacedData);
+        }
+
+        markers.forEach(marker => {
+             
+            if(!replacedData.includes(marker))
+            {
+                pending--;
+                return; //hier wie continue
             }
-            fs.readFile(path.join(__dirname, '../templates', 'test.html'), 'utf8', (testErr, testContent) => {
-                if (testErr){ 
-                    testContent = '';
-                    console.log("Error in replaceMiddleware: could not find file test.html");
+
+            fs.readFile(replacements[marker], 'utf8', (fileErr, fileContent) => {
+                if (fileErr) {
+                    console.log(`Error in replaceMiddleware: could not find file for marker ${marker}`);
                 }
-                const replaced = data.replace('<!-- test -->', testContent);
-                res.setHeader('Content-Type', 'text/html');
-                res.send(replaced);
+                else {
+                    replacedData = replacedData.replace(marker, fileContent);
+                }
+                pending--;
+                if (pending <= 0) {
+                    res.setHeader('Content-Type', 'text/html');
+                    res.send(replacedData);
+                }
             });
         });
-    } else {
-        next();
-    }
+    });
 }
 
 module.exports = replaceMiddleware;
