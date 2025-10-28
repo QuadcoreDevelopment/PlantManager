ce7f23ac498bf2d29fa56aaf3bf068ecdcfc53da
CoderTobi <77673526+CoderTobi@users.noreply.github.com>
2025-10-28 23:37:23 +0100

    #41 added validation helpers for activityID and exception to JSON

--- backend/services/validationHelper.js
+++ backend/services/validationHelper.js
@@ -1,6 +1,7 @@
 const plantsDao = require('../dao/plantsDao.js');
+const activitiesDao = require('../dao/activitiesDao.js');
 
-// plant_id exists validation function
+// plant exists validation function
 module.exports.validatePlantIDExists = (value,{req}) => {
     const plantsDaoInstance = new plantsDao(req.app.locals.dbConnection);
     console.log('Validating plant_id existence: ' + value);
@@ -9,3 +10,25 @@ module.exports.validatePlantIDExists = (value,{req}) => {
     }
     return true;
 };
+
+// activity exists validation function
+module.exports.validateActivityIDExists = (value,{req}) => {
+    const activitiesDaoInstance = new activitiesDao(req.app.locals.dbConnection);
+    console.log('Validating activity_id existence: ' + value);
+    if (!activitiesDaoInstance.exists(value)) {
+      throw new Error('Activity with the given ID does not exist');
+    }
+    return true;
+}
+
+/**
+ * Converts an exception to a JSON object.
+ * @param {Error} ex The exception to convert
+ * @returns {Object} JSON object representing the exception
+ */
+module.exports.exceptionToJson = (ex) => {
+    return { 
+      'msg': "An error occurred", 
+      'errorMsg': ex.message
+    };
+}
