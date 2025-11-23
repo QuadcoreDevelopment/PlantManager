236a366a9b6af22b5532918f569818e74a5286da
CoderTobi <77673526+CoderTobi@users.noreply.github.com>
2025-11-23 21:09:17 +0100

    Updated plantsDao - uses helper now to calculate days between - #70 updated according to new DB schema - moved extendPlantJSON - made date handling consistent (always ISO string) - added JS Doc - #76 changed var to let

--- backend/services/plants.js
+++ backend/services/plants.js
@@ -2,93 +2,12 @@ const helper = require('../helper.js');
 const validationHelper = require('./validationHelper.js');
 const express = require('express');
 const plantsDao = require('../dao/plantsDao.js');
-const activitiesDao = require('../dao/activitiesDao.js');
 var serviceRouter = express.Router();
 const { body, param, matchedData, validationResult } = require('express-validator');
 
 console.log('- Service Plants');
 
 // TODO #70 Update this file according to new DB schema
-
-// TODO #70 Move this function to plantsDao.js
-function extendPlantJSON(json,activitiesDaoInstance) {
-    //const plantDaoInstance = new plantsDao(request.app.locals.dbConnection);
-    //const activitiesDaoInstance = new activitiesDao(request.app.locals.dbConnection);
-
-    // Berechnung watering_interval_calculated
-    let watering_interval_calculated = json.watering_interval + json.watering_interval_offset;
-
-    // Datum letztes Bewässern ermitteln 
-    let arrWat = activitiesDaoInstance.loadByPlantIdAndType(json.plant_id,0);
-    let last_watered = null;
-    if(helper.isArray(arrWat))
-    {
-        if (helper.isArrayEmpty(arrWat))
-        {
-            // No elements = empty array
-            last_watered = new Date(json.added);
-        }
-        else
-        {
-            // 2 or more elements = array
-            last_watered = new Date(arrWat[0].date);
-        }
-    }
-    else if (!helper.isUndefined(arrWat))
-    {
-        // exactly one element
-        last_watered = new Date(arrWat.date);
-    }
-    else{
-        // something went wrong
-        last_watered = new Date(json.added);
-    }
-    
-
-    // Berechnung days_since_watering
-    const currentDate = new Date();
-    let ms_since_watering = currentDate - last_watered;
-    let days_since_watering = Math.floor(ms_since_watering / (1000 * 60 * 60 * 24));
-
-    // Berechnung days_until_watering
-    // Erst berechnen wann das nächste mal gegossen werden muss
-    const watering_due = new Date(last_watered.getTime() + watering_interval_calculated * 24 * 60 * 60 * 1000);
-    // ms von heute von ms vom gießdatum abziehen und runden --> negativ heisst ueberfaellig
-    let ms_until_watering = watering_due - currentDate;
-    let days_until_watering = Math.floor(ms_until_watering / (1000 * 60 * 60 * 24)) +1;
-
-    //Bestimmung repotted
-    let arrPot = activitiesDaoInstance.loadByPlantIdAndType(json.plant_id,1);
-    let repotted = null;
-    if(helper.isArray(arrPot))
-    {
-        if (helper.isArrayEmpty(arrPot))
-        {
-            // No elements = empty array
-            repotted = json.added;
-        }
-        else
-        {
-            // 2 or more elements = array
-            repotted = arrPot[0].date;
-        }
-    }
-    else if (!helper.isUndefined(arrPot))
-    {
-        // exactly one element
-        repotted = arrPot.date;
-    }
-    else{
-        // something went wrong
-        repotted = json.added;
-    }
-
-    //JSON erweitern
-    json.watering_interval_calculated = watering_interval_calculated;
-    json.days_since_watering = days_since_watering;
-    json.days_until_watering = days_until_watering;
-    json.repotted = repotted;
-}
 
 serviceRouter.get('/plants/get/:plant_id', 
     param("plant_id").isInt({min:0}).bail().toInt().custom(validationHelper.validatePlantIDExists),
